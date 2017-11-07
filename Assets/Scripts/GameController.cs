c8a342a064d87045c5023168f413fc4c4190a64c
Jack Muratore <jackmuratore@gmail.com>
2017-11-06 22:45:03 -0500

    Transfer to git

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,93 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+	public Text[] ButtonList;
+	public Text GameOverText;
+	public GameObject GameOverPanel;
+
+	private string _playerSide;
+	private int _moveCount;
+
+	void Awake()
+	{
+		_playerSide = "X";
+		_moveCount = 0;
+		GameOverPanel.SetActive(false);
+		SetGameControllerReferenceOnButtons();
+	}
+
+	void SetGameControllerReferenceOnButtons()
+	{
+		for (var i = 0; i < ButtonList.Length; i++)
+		{
+			ButtonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+		}
+
+	}
+
+	public string GetPlayerSide()
+	{
+		return _playerSide;
+	}
+
+	public void EndTurn()
+	{
+		_moveCount++;
+		if ((ButtonList[0].text == _playerSide && ButtonList[1].text == _playerSide && ButtonList[2].text == _playerSide)
+		    || (ButtonList[3].text == _playerSide && ButtonList[4].text == _playerSide && ButtonList[5].text == _playerSide)
+		    || (ButtonList[6].text == _playerSide && ButtonList[7].text == _playerSide && ButtonList[8].text == _playerSide)
+		    || (ButtonList[0].text == _playerSide && ButtonList[3].text == _playerSide && ButtonList[6].text == _playerSide)
+		    || (ButtonList[1].text == _playerSide && ButtonList[4].text == _playerSide && ButtonList[7].text == _playerSide)
+		    || (ButtonList[2].text == _playerSide && ButtonList[5].text == _playerSide && ButtonList[8].text == _playerSide)
+		    || (ButtonList[0].text == _playerSide && ButtonList[4].text == _playerSide && ButtonList[8].text == _playerSide)
+		    || (ButtonList[2].text == _playerSide && ButtonList[4].text == _playerSide && ButtonList[6].text == _playerSide))
+
+		{
+			GameOver();
+		}
+		ChangeSides();
+		if (_moveCount >= 9)
+		{
+			SetGameOverText("It's a draw!");
+		}
+	}
+
+	void SetGameOverText(string value)
+	{
+		GameOverPanel.SetActive(true);
+		GameOverText.text = value;
+	}
+
+	public void RestartGame()
+	{
+		_playerSide = "X";
+		_moveCount = 0;
+		GameOverPanel.SetActive(false);
+		SetBoardInteractable(true);
+	}
+
+	void SetBoardInteractable(bool toggle)
+	{
+		foreach (var button in ButtonList)
+		{
+			button.GetComponentInParent<Button>().interactable = toggle;
+			button.text = "";
+		}
+	}
+
+	void GameOver()
+	{
+		SetBoardInteractable(false);
+		SetGameOverText(_playerSide + " Wins!");
+
+	}
+
+	void ChangeSides()
+	{
+		_playerSide = _playerSide == "X" ? "O" : "X";
+	}
+}
---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+
+    public Button button;
+    public Text buttonText;
+
+    private GameController _gameController;
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        _gameController = controller;
+    }
+    public void SetSpace()
+    {
+        Debug.Log("Set space to " + _gameController.GetPlayerSide());
+        buttonText.text = _gameController.GetPlayerSide();
+        button.interactable = false;
+        _gameController.EndTurn();
+    }
+}
